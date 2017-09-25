be3bd9800d317e16ea184b1b523907c1f6dfbda9
DESKTOP-AEUJEBD\r4d3k <r4d3ksz@gmail.com>
2017-09-25 17:47:19 +0200

    Init

---
+++ ElPlaga/ElPlaga.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ElPlaga.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
